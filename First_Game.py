d4ccfa01ab9d077893c6200b460d71c460442b61
TIanMICloud <tianmicloud@163.com>
2022-01-05 16:10:18 +0800

    新增一些乐趣额

--- First_Game.py
+++ First_Game.py
@@ -1,9 +1,22 @@
 """用python设计的第一个小程序"""
-temp = input("不妨猜猜闵军心中想的数字：")
-guess = int(temp)
-if guess == '9':
-    print("哇晒！你是闵军心里的小蛔虫哈！")
-    print("猜对了，可惜不给你奖励！")
-else:
-    print("猜错了哦，我心中想的数字是9！")
+import random
+
+answer = random.randint(1,10)
+const = 3
+
+
+while const > 0:
+    temp = input("不妨猜猜闵军心中想的数字,你有三次机会哦！：")
+    guess = int(temp)
+    if guess == answer:
+        print("哇晒！你是闵军心里的小蛔虫哈！")
+        print("猜对了，可惜不给你奖励！")
+        break
+    else:
+        if guess < answer:
+            print("小了哦！")
+        else:
+            print("大了哦！")
+        const -= 1
+
 print("游戏结束了！不玩了哦！")
